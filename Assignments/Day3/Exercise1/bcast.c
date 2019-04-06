d5eb438b7f13995fdf2a3594ff8616f2090052d3
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-04-07 00:44:47 +0200

    Update bcast.c

--- Assignments/Day3/Exercise1/bcast.c
+++ Assignments/Day3/Exercise1/bcast.c
@@ -4,24 +4,22 @@
 
 int main( int argc, char * argv[] ){
 
-//  int imesg = 0;
   int rank = 0; // store the MPI identifier of the process
   int npes = 1; // store the number of MPI processes
-  size_t size = 1024 * 1024 * 1024;
-  int n_elem = size / sizeof(int);
-  int* buffer = (int*)malloc(size);
+  size_t size = 1024 * 1024 * 1024;  // 1 GB of size for the buffer
+  int n_elem = size / sizeof(int);  // number of elements in the buffer
+  int* buffer = (int*)malloc(size); // allocate the buffer
 
   MPI_Init( &argc, &argv );
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
   MPI_Comm_size( MPI_COMM_WORLD, &npes );
-
+  // initialize the buffer
   for (int i=0; i < n_elem; ++i) {
       buffer[i] = -npes;
   }
 
-//  imesg = rank;
   fprintf( stderr, "\nBefore Bcast operation I am process %d of %d MPI processes and my message content is: %d\n", rank, npes, buffer[npes - 1] );
-
+  // perform 1:n communication from process 0
   MPI_Bcast( buffer, n_elem, MPI_INT, 0, MPI_COMM_WORLD );
 
   fprintf( stderr, "\nAfter Bcast operation I am process %d of %d MPI processes and my message content is: %d\n", rank, npes, buffer[npes - 1] );
