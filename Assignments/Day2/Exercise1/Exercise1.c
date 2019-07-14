57dadb2057d4e56b72a443e0d00c2fed99ab750f
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 00:05:22 +0200

    Update Exercise1.c

--- Assignments/Day2/Exercise1/Exercise1.c
+++ Assignments/Day2/Exercise1/Exercise1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <omp.h>
 
+/* Main function */
 
 int main(int argc, char* argv[]) {
 
@@ -12,53 +13,53 @@ int main(int argc, char* argv[]) {
     double avg_job_duration;
 
     printf("Test with critical:\n");
-// start of the OpenMP parallel region; test with 'critical'
-#pragma omp parallel
-{
-    double curr;
-    double tstart = omp_get_wtime();
+    // start of the OpenMP parallel region; test with 'critical'
+    #pragma omp parallel
+    {
+        double curr;
+        double tstart = omp_get_wtime();
 
-    // work-sharing construct to split the computation of the different
-    // rectangles
-    #pragma omp for
-    for (int i=0; i < N; ++i) {
-        curr = h * (i + 0.5);
-        #pragma omp critical    // protect against a race condition
-        global_result += 1.0 / (1.0 + curr*curr);
+        // work-sharing construct to split the computation of the different
+        // rectangles
+        #pragma omp for
+        for (int i=0; i < N; ++i) {
+            curr = h * (i + 0.5);
+            #pragma omp critical    // protect against a race condition
+            global_result += 1.0 / (1.0 + curr*curr);
+        }
+        // implicit barrier upon exit of the work-sharing construct
+        double job_duration = omp_get_wtime() - tstart;
+        #pragma omp atomic
+        avg_job_duration += job_duration / omp_get_num_threads();   // omp_get_num_threads() does not have race conditions
     }
-    // implicit barrier upon exit of the work-sharing construct
-    double job_duration = omp_get_wtime() - tstart;
-    #pragma omp atomic
-    avg_job_duration += job_duration / omp_get_num_threads();   //omp_get_num_threads() does not have race conditions
-}
     global_result *= 4.0 * h;
 
     printf("Result: %f\n", global_result);
     printf("Time elapsed: %f\n", avg_job_duration);
-   // re-initialize
-   global_result = 0.0;
-   avg_job_duration = 0.0;
-   printf("Test with atomic:\n");
+    // re-initialize
+    global_result = 0.0;
+    avg_job_duration = 0.0;
+    printf("Test with atomic:\n");
 
-// start of the OpenMP parallel region; test with 'atomic'
-#pragma omp parallel
-{
-    double curr;
-    double tstart = omp_get_wtime();
+    // start of the OpenMP parallel region; test with 'atomic'
+    #pragma omp parallel
+    {
+        double curr;
+        double tstart = omp_get_wtime();
 
-    // work-sharing construct to split the computation of the different
-    // rectangles
-    #pragma omp for
-    for (int i=0; i < N; ++i) {
-        curr = h * (i + 0.5);
-        #pragma omp atomic    // protect against a race condition
-        global_result += 1.0 / (1.0 + curr*curr);
+        // work-sharing construct to split the computation of the different
+        // rectangles
+        #pragma omp for
+        for (int i=0; i < N; ++i) {
+            curr = h * (i + 0.5);
+            #pragma omp atomic    // protect against a race condition
+            global_result += 1.0 / (1.0 + curr*curr);
+        }
+        // implicit barrier upon exit of the work-sharing construct
+        double job_duration = omp_get_wtime() - tstart;
+        #pragma omp atomic
+        avg_job_duration += job_duration / omp_get_num_threads();   //omp_get_num_threads() does not have race conditions
     }
-    // implicit barrier upon exit of the work-sharing construct
-    double job_duration = omp_get_wtime() - tstart;
-    #pragma omp atomic
-    avg_job_duration += job_duration / omp_get_num_threads();   //omp_get_num_threads() does not have race conditions
-}
     global_result *= 4.0 * h;
 
     printf("Result: %f\n", global_result);
@@ -69,24 +70,24 @@ int main(int argc, char* argv[]) {
     avg_job_duration = 0.0;
     printf("Test with reduction:\n");
 
-// start of the OpenMP parallel region; test with reduction, as seen in the following line
-#pragma omp parallel reduction(+:global_result)
-{
-     double curr;
-     double tstart = omp_get_wtime();
+    // start of the OpenMP parallel region; test with reduction, as seen in the following line
+    #pragma omp parallel reduction(+:global_result)
+    {
+        double curr;
+        double tstart = omp_get_wtime();
 
-     // work-sharing construct to split the computation of the different
-     // rectangles
-     #pragma omp for
-     for (int i=0; i < N; ++i) {
-         curr = h * (i + 0.5);
-         global_result += 1.0 / (1.0 + curr*curr);
-     }
-     // implicit barrier upon exit of the work-sharing construct
-     double job_duration = omp_get_wtime() - tstart;
-     #pragma omp atomic
-     avg_job_duration += job_duration / omp_get_num_threads();
-}
+        // work-sharing construct to split the computation of the different
+        // rectangles
+        #pragma omp for
+        for (int i=0; i < N; ++i) {
+            curr = h * (i + 0.5);
+            global_result += 1.0 / (1.0 + curr*curr);
+        }
+        // implicit barrier upon exit of the work-sharing construct
+        double job_duration = omp_get_wtime() - tstart;
+        #pragma omp atomic
+        avg_job_duration += job_duration / omp_get_num_threads();
+    }
 
     global_result *= 4.0 * h;
     printf("Result: %f\n", global_result);
