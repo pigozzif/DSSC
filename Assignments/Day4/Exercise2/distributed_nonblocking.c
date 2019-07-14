a9536778dbb4a94ecdd0d4a48119971b4287184b
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-14 13:19:40 +0200

    Update distributed_nonblocking.c

--- Assignments/Day4/Exercise2/distributed_nonblocking.c
+++ Assignments/Day4/Exercise2/distributed_nonblocking.c
@@ -92,7 +92,7 @@ int main(int argc, char* argv[]) {
             // do the same as the above for loop, but writing on file
             for (int curr_rank=1; curr_rank < npes; ++curr_rank) {
                 MPI_Irecv(recv_buf, local_N * N, MPI_INT, curr_rank, 101, MPI_COMM_WORLD, &request);
-                write_matrix(Mat, local_N, N, fp);
+                write_matrix(Mat, local_N, N, fp);  // write
                 MPI_Wait(&request, MPI_STATUS_IGNORE);  // otherwise we cannot be sure we can swap the pointers
                 swap(&recv_buf, &Mat);
             }
