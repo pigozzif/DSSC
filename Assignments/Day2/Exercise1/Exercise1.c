b5e2905399f2ba0ecec313b0e645a4356c9c35b0
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 00:26:06 +0200

    Update Exercise1.c

--- Assignments/Day2/Exercise1/Exercise1.c
+++ Assignments/Day2/Exercise1/Exercise1.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <omp.h>
 
+/* this function returns the seconds elapsed since the start of the Unix epoch on January 1, 1970 */
+
+double seconds() {
+  struct timeval tmp;
+  double sec;
+  gettimeofday(&tmp, (struct timezone *)0);
+  sec = tmp.tv_sec + ((double)tmp.tv_usec) / 1000000.0;
+  return sec;
+}
+
 /* Main function */
 
 int main(int argc, char* argv[]) {
