5432afb475096bacab41e27dce444a3f2c7689c3
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 10:57:59 +0200

    Update script.sh

--- Assignments/Day2/Exercise1/script.sh
+++ Assignments/Day2/Exercise1/script.sh
@@ -4,5 +4,7 @@ for nthreads in 1 2 4 8 16 20; do
     export OMP_NUM_THREADS=${nthreads}
     make
     echo ${nthreads}
-    ./exercise1.x
+    ./critical.x
+    ./atomic.x
+    ./reduction.x
 done
