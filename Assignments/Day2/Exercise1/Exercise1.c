4f499220e12976c437bf281ac8c09c94cfd2c855
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 10:29:50 +0200

    Update Exercise1.c

--- Assignments/Day2/Exercise1/Exercise1.c
+++ Assignments/Day2/Exercise1/Exercise1.c
@@ -21,17 +21,16 @@ int main(int argc, char* argv[]) {
     double global_result = 0.0;
     int N = 1000000000;
     double h = 1.0 / N;  // step
-    //double avg_job_duration;
 
     printf("Test with critical:\n");
     
     double start = seconds();
+
     // start of the OpenMP parallel region; test with 'critical'
     #pragma omp parallel
     {
         double curr, local_result;
-//        double tstart = omp_get_wtime();
-        printf("Num of threads:%d\n", omp_get_num_threads());
+        
         // work-sharing construct to split the computation of the different
         // rectangles
         #pragma omp for
@@ -40,6 +39,7 @@ int main(int argc, char* argv[]) {
             //#pragma omp critical    // protect against a race condition
             local_result += 1.0 / (1.0 + curr*curr);
         }
+        #pragma omp critical
         global_result += local_result;
         // implicit barrier upon exit of the work-sharing construct
         //double job_duration = omp_get_wtime() - tstart;
@@ -47,6 +47,7 @@ int main(int argc, char* argv[]) {
         //avg_job_duration += job_duration / omp_get_num_threads();   // omp_get_num_threads() does not have race conditions
     }
     global_result *= 4.0 * h;
+    
     double end = seconds();
     printf("Result: %f\n", global_result);
     printf("Time elapsed: %f\n", end - start);
@@ -69,11 +70,9 @@ int main(int argc, char* argv[]) {
             //#pragma omp atomic    // protect against a race condition
             local_result += 1.0 / (1.0 + curr*curr);
         }
-        global_result += local_result;
         // implicit barrier upon exit of the work-sharing construct
-        //double job_duration = omp_get_wtime() - tstart;
-        //#pragma omp atomic
-        //avg_job_duration += job_duration / omp_get_num_threads();   //omp_get_num_threads() does not have race conditions
+        #pragma omp atomic
+        global_result += local_result;
     }
     global_result *= 4.0 * h;
     end = seconds();
