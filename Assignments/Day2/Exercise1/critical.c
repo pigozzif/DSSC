e4126a34aaa270f96a7cc3e835091f8785b7cf82
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 10:49:04 +0200

    Update and rename Exercise1.c to critical.c

--- Assignments/Day2/Exercise1/critical.c
+++ Assignments/Day2/Exercise1/critical.c
@@ -19,38 +19,32 @@ int main(int argc, char* argv[]) {
 
     // initialization of global variables
     double global_result = 0.0;
-    double local_result = 0.0;
-    double curr = 0.0;
     int N = 1000000000;
     double h = 1.0 / N;  // step
 
-    printf("Test with critical:\n");
+    printf("\nTest with critical:\n");
 
     double start = seconds();
 
-    // start of the OpenMP parallel region; test with 'critical'
-    #pragma omp parallel firstprivate(local_result, curr)
+    // start of the OpenMP parallel region
+    #pragma omp parallel
     {
-        //double curr, local_result;
+        double curr, local_result;
         printf("Number of threads:%d\n", omp_get_num_threads());
         // work-sharing construct to split the computation of the different
         // rectangles
         #pragma omp for
         for (int i=0; i < N; ++i) {
             curr = h * (i + 0.5);
-            //#pragma omp critical    // protect against a race condition
             local_result += 1.0 / (1.0 + curr*curr);
         }
         #pragma omp critical
         global_result += local_result;
-        // implicit barrier upon exit of the work-sharing construct
-        //double job_duration = omp_get_wtime() - tstart;
-        //#pragma omp atomic
-        //avg_job_duration += job_duration / omp_get_num_threads();   // omp_get_num_threads() does not have race conditions
     }
     global_result *= 4.0 * h;
     
     double end = seconds();
+    
     printf("Result: %f\n", global_result);
     printf("Time elapsed: %f\n", end - start);
     // re-initialize
