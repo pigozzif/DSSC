b1cd8b29d1641e1630c6866bebc1a8f2fc81cfe8
pigozzif <43779861+pigozzif@users.noreply.github.com>
2019-07-15 15:01:53 +0200

    Update ring.c

--- Assignments/Day4/Exercise1/ring.c
+++ Assignments/Day4/Exercise1/ring.c
@@ -24,46 +24,51 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &npes);
 
+    // init local buffers
+    // X = me
+    // sum = X
     #ifndef VECTOR
-    X = rank;
-    sum = 0;
+    // scalars
+    int X = rank;
+    int sum = 0;
     #else
     // allocate local buffers
     int* X = (int*)malloc(N * sizeof(int));
     int* sum = (int*)malloc(N * sizeof(int));
 
-    // init local buffers
-    // X = me
-    // sum = X
+    // fill local buffers
     for (int i=0; i < N; ++i) {
         X[i] = rank;
         sum[i] = 0;
     }
     #endif
     
-    // iterate over all the processes:
+    #ifndef VECTOR
+    // iterate over all the processes
+    for (int iter=0; iter < npes; ++iter) {
+        // 1. send X to the right
+        MPI_Isend(&X, N, MPI_INT, PROC_RIGHT(rank, npes), 101, MPI_COMM_WORLD, &request);  // non-blocking send
+        // 2. perform computation: sum = sum + X
+        sum += X;
+        // 3. recv X from left
+        MPI_Wait(&request, MPI_STATUS_IGNORE);
+        MPI_Recv(&X, N, MPI_INT, PROC_LEFT(rank, npes), 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    
+    #else
+    // iterate over all the processes
     for (int iter=0; iter < npes; ++iter) {
         // 1. send X to the right
         MPI_Isend(X, N, MPI_INT, PROC_RIGHT(rank, npes), 101, MPI_COMM_WORLD, &request);  // non-blocking send
         // 2. perform computation: sum = sum + X
-        #ifndef VECTOR
-        sum += X;
-        #else
         for (int i=0; i < N; ++i) {
             sum[i] += X[i];
         }
-        #endif
         // 3. recv X from left
         MPI_Wait(&request, MPI_STATUS_IGNORE);
         MPI_Recv(X, N, MPI_INT, PROC_LEFT(rank, npes), 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    // take care of last computation
-    #ifndef VECTOR
-    sum += X;
-    #else
-    for (int i=0; i < N; ++i) {
-            sum[i] += X[i];
-        }
+    
     #endif
     
     // check result
@@ -77,9 +82,11 @@ int main(int argc, char* argv[]) {
     printf("\n");
     #endif
     
+    #ifdef VECTOR
     // deallocate and finalize
     free(X);
     free(sum);
+    #endif
     
     MPI_Finalize();
 
